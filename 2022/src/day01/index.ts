e348dee21e5e84b96b1b97eb85e5a05642d7c572
Teofanis Papadopulos <teos_97@hotmail.com>
2022-12-01 17:56:29 +0000

    day1

--- 2022/src/day01/index.ts
+++ 2022/src/day01/index.ts
@@ -3,7 +3,11 @@ import run from "aocrunner";
 const parseInput = (rawInput: string) => {
   return rawInput
     .split("\n")
-    .map(Number)
+    .map((num) => {
+      const parsed = parseInt(num, 10);
+      if (isNaN(parsed)) return 0;
+      return parsed;
+    })
     .reduce((acc, curr, currIndex) => {
       const lineIndex = currIndex === 0 ? 0 : acc.length - 1;
       const currentLineArray = acc[lineIndex] || [];
@@ -14,45 +18,69 @@ const parseInput = (rawInput: string) => {
       currentLineArray.push(curr);
       acc[lineIndex] = currentLineArray;
       return acc;
-    }, new Array<Array<Number>>());
+    }, new Array<Array<number>>());
 };
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const summedCaloriesPerElf = input.map((elf) =>
-    elf.reduce((acc, curr) => Number(acc) + Number(curr), 0),
+    elf.reduce((acc, curr) => acc + curr, 0),
   );
-  // @ts-ignore
   return Math.max(...summedCaloriesPerElf);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
   const summedCaloriesPerElf = input
-    .map((elf) => elf.reduce((acc, curr) => Number(acc) + Number(curr), 0))
-    .sort((a, b) => Number(b) - Number(a));
-  // @ts-ignore
-  return  summedCaloriesPerElf
-    .slice(0, 3)
-    .reduce((acc, curr) => Number(acc) + Number(curr), 0) as number;
+    .map((elf) => elf.reduce((acc, curr) => acc + curr, 0))
+    .sort((a, b) => b - a);
+  return summedCaloriesPerElf.slice(0, 3).reduce((acc, curr) => acc + curr, 0);
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        5213
+        1234
+
+        4251
+        4215
+        73
+
+        2313
+        5000
+
+        `,
+        expected: 8539,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+        5213
+        1234
+
+        4251
+        4215
+        73
+
+        2313
+        5000
+        
+        5125
+        2541
+        23
+
+        3123
+        1515
+        `,
+        expected: 23541,
+      },
     ],
     solution: part2,
   },
